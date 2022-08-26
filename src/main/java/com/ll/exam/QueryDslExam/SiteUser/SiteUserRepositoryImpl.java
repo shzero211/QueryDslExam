ba9612124ca6473d77abb236768e76d34bfd4bea
SangHun <shzero211@naver.com>
2022-08-26 14:50:58 +0900

    feat:QueryDsl QSiteUser 간소화

--- src/main/java/com/ll/exam/QueryDslExam/SiteUser/SiteUserRepositoryImpl.java
+++ src/main/java/com/ll/exam/QueryDslExam/SiteUser/SiteUserRepositoryImpl.java
@@ -3,15 +3,17 @@ package com.ll.exam.QueryDslExam.SiteUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import static com.ll.exam.QueryDslExam.SiteUser.QSiteUser.*;
+
 @RequiredArgsConstructor
 public class SiteUserRepositoryImpl implements  SiteUserRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
     @Override
     public SiteUser getQslUser(Long id) {
         return jpaQueryFactory
-                .select(QSiteUser.siteUser)
-                .from(QSiteUser.siteUser)
-                .where(QSiteUser.siteUser.id.eq(id))
+                .select(siteUser)
+                .from(siteUser)
+                .where(siteUser.id.eq(id))
                 .fetchOne();
     }
 }
