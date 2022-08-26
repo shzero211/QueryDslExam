8700fcc71b80500253eca6a53a9d318b7713eb08
SangHun <shzero211@naver.com>
2022-08-26 12:58:29 +0900

    셋팅

---
+++ src/main/java/com/ll/exam/QueryDslExam/QueryDslExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.QueryDslExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/QueryDslExam/QueryDslExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.QueryDslExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
